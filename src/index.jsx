3433659b7f39e2ab8503625037d2fcd502aa5c8f
Armann Sigurdsson <armann.sigurdsson@fast2.se>
2018-02-09 22:52:28 +0100

    Fetch videos with youtube api

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import API_KEY from './config/youtube_api_key';
 import SearchBar from './components/search_bar';
 
-const App = () => (
-  <div>
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    this.doSearch('surfboards');
+  }
+
+  doSearch(term) {
+    YTSearch({ key: API_KEY, term }, (videos) => {
+      this.setState({ videos });
+      console.log(videos);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
