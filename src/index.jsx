afb7e9d9e83b06ab629a6037510a1ca64876cf9d
Armann Sigurdsson <armann.sigurdsson@fast2.se>
2018-03-05 23:02:39 +0100

    Add clickable video list items with styling

--- src/index.jsx
+++ src/index.jsx
@@ -3,18 +3,28 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import API_KEY from './config/youtube_api_key';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+  }
+
+  componentDidMount() {
     this.doSearch('surfboards');
   }
 
   doSearch(term) {
     YTSearch({ key: API_KEY, term }, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
     });
   }
 
@@ -22,7 +32,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
